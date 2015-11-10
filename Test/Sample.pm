6a81214c9559086288fa8bd55af86a143b61f627
Marek Marecki <marek.marecki@posbit.pl>
2015-11-10 18:49:09 +0100

    More tests

--- Test/Sample.pm
+++ Test/Sample.pm
@@ -6,9 +6,13 @@ use Test::More;
 use Test::Framework;
 
 
+# create test class
 my $test_class = Test::Framework->new('FrameworkTest');
 
 
+######################################################################
+# POSITIVE TYPE ASSERTIONS
+#
 $test_class->register_test('test_assert_not_typeof', sub {
     my $framework = shift;
     $framework->assert_not_typeof('Test::Foo', undef);
@@ -21,22 +25,45 @@ $test_class->register_test('test_assert_not_typeof', sub {
     $framework->assert_not_typeof('Test::Foo', (0, 1, 2, 3));
     $framework->assert_not_typeof('Test::Foo', (a => 0, b => 1));
 });
-
 $test_class->register_test_assert_not_typeof('test_reigster_test_assert_not_typeof', 'Test::Foo', sub {
     my $framework = shift;
     return $framework;
 });
 
+
+######################################################################
+# NEGATIVE TYPE ASSERTIONS
+#
 $test_class->register_test('test_assert_typeof', sub {
     my $framework = shift;
     $framework->assert_typeof('Test::Framework', $framework);
 });
-
 $test_class->register_test_assert_typeof('test_reigster_test_assert_typeof', 'Test::Framework', sub {
     my $framework = shift;
     return $framework;
 });
 
 
+######################################################################
+# SIMPLE BOOLEAN ASSERTIONS
+#
+$test_class->register_test('test_positive_boolean_assertion', sub {
+    my $framework = shift;
+    $framework->assert_true(1);
+});
+$test_class->register_test_assert_true('test_register_positive_boolean_assertion', sub {
+    return 'true-looking value';
+});
+
+$test_class->register_test('test_negative_boolean_assertion', sub {
+    my $framework = shift;
+    $framework->assert_false(0);
+});
+$test_class->register_test_assert_false('test_register_negative_boolean_assertion', sub {
+    return '';  # false-looking value
+});
+
+
+# run tests
 $test_class->run(@ARGV);
 done_testing();
